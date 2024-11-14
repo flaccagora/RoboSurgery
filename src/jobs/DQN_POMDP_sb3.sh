87cc3690fc03ce1abf81fc85bb2b46c9ae57f6a6
flaccagora <matteo.nunziante@gmail.com>
2024-11-14 12:46:47 +0100

    sbatch dqn pomdp sb3

--- src/jobs/DQN_POMDP_sb3.sh
+++ src/jobs/DQN_POMDP_sb3.sh
@@ -5,8 +5,8 @@
 #SBATCH -N 1                # 1 node
 #SBATCH --ntasks-per-node=4 # 4 tasks out of 112
 #SBATCH --mem=64000          # memory per node out of 494000MB (481GB)
-#SBATCH --job-name=DQN_POMDP
-#SBATCH --output=DQN_POMDP.out 
+#SBATCH --job-name=DQN_POMDPsb3
+#SBATCH --output=DQN_POMDPsb3.out 
 
 date
 
