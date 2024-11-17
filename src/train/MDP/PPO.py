762d5e3d6fb53e7eca87f953bf0c93770316aafe
flaccagora <matteo.nunziante@gmail.com>
2024-11-17 15:13:39 +0100

    ppo

--- src/train/MDP/PPO.py
+++ src/train/MDP/PPO.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     parser.add_argument("--learning_rate", type=float, default=0.0003)
     parser.add_argument("--batch_size", type=int, default=2000)
     parser.add_argument("--n_steps", type=int, default=2000)
-    parser.add_argument("--total_timesteps", type=int, default=100000)
+    parser.add_argument("--total_timesteps", type=int, default=1000000)
     
     args = parser.parse_args()
 
