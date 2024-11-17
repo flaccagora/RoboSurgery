3b732b4f0f3e050a024e2af84491496f952c9e1e
flaccagora <matteo.nunziante@gmail.com>
2024-11-17 11:10:18 +0100

    orfeo sbatch

---
+++ src/jobs/orfeo/DQNsb3_MDP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A dssc
+#SBATCH -p EPYC
+#SBATCH --time 02:00:00     # format: HH:MM:SS
+#SBATCH -N 1                # 1 node
+#SBATCH --ntasks-per-node=4 # 4 tasks out of 112
+#SBATCH --job-name=DQNsb3_MDP
+#SBATCH --output=DQNsb3_MDP.out 
+
+date
+
+conda deactivate
+
+cd ~/RoboSurgery
+source .rob/bin/activate
+
+cd ./src
+
+export PYTHONPATH=$(pwd)
+export WANDB_MODE=offline
+
+python3 train/MDP/DQN_sb3.py --learning_rate=0.001
+
+date
