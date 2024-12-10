8134813061dbaad1d4b5eb1e073523c3c350475e
flaccagora <matteo.nunziante@gmail.com>
2024-12-10 16:18:28 +0100

    newenv train ready

--- src/train/MDP/DQN_newenv.py
+++ src/train/MDP/DQN_newenv.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import pygame
-import time
 import gymnasium as gym
-from gymnasium.spaces import Dict, Discrete, Box
-from collections import OrderedDict
 from environment.env import ObservableDeformedGridworld
 
 
