5f20333721daf3fcba29ff22422f708b56e2a14e
flaccagora <matteo.nunziante@gmail.com>
2024-12-11 13:21:41 +0100

    newenv train ready

--- src/train/MDP/DQN_newenv.py
+++ src/train/MDP/DQN_newenv.py
@@ -122,7 +122,7 @@ def train_dqn(args):
 
     net_arch=[128, 128, 128]
     if args.run_id is not None:
-        last_checkpoint = find_last_checkpoint(f"DQN_continous_{args.run_id}")
+        last_checkpoint = find_last_checkpoint(f"agents/pretrained/MDP/DQN_continous_{args.run_id}")
         model = DQN.load(last_checkpoint,env=env,)
     else:
         model = DQN("MultiInputPolicy",env,batch_size=batch_size,gamma=gamma, 
