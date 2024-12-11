272b286e25294d113af47cf35ce1609af2338161
flaccagora <matteo.nunziante@gmail.com>
2024-12-11 13:21:17 +0100

    newenv train ready

--- src/train/MDP/DQN_newenv.py
+++ src/train/MDP/DQN_newenv.py
@@ -1,6 +1,8 @@
 import numpy as np
 import gymnasium as gym
 from environment.env import ObservableDeformedGridworld
+import os
+import re
 
 
 from stable_baselines3 import DQN
@@ -12,6 +14,27 @@ obstacles = [((0.14625, 0.3325), (0.565, 0.55625)),
              ((0.0075, 0.00125), (0.99875, 0.04)), 
              ((0.98875, 0.0075), (0.99875, 1.0)), 
              ((0.00125, 0.9825), (0.99875, 1.0))]
+
+def find_last_checkpoint(directory):
+    # Define the regex pattern to match the checkpoint file names
+    pattern = re.compile(r"rl_model_(\d+)_steps")
+
+    last_checkpoint = None
+    max_steps = -1
+
+    # Iterate through all files in the directory
+    for filename in os.listdir(directory):
+        match = pattern.match(filename)
+        if match:
+            # Extract the step number
+            steps = int(match.group(1))
+            # Check if this is the highest step count seen so far
+            if steps > max_steps:
+                max_steps = steps
+                last_checkpoint = filename
+
+    return os.path.splitext(last_checkpoint)[0] if last_checkpoint else None
+
 
 
 def train_dqn(args):
@@ -25,6 +48,7 @@ def train_dqn(args):
     target_update = args.target_update
     gamma = args.gamma
     render_mode = args.render_mode
+    run_id = args.run_id
 
     config = {
         "policy_type": "MultiInputPolicy",
@@ -38,19 +62,30 @@ def train_dqn(args):
         "shear range":None,
         "stretch range":None
     }
-    run = wandb.init(
-        project="DQNsb3 - MDP - ObservableDeformedGridworld",
-        config=config,
-        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-        monitor_gym=True,  # auto-upload the videos of agents playing the game
-        save_code=True,  # optional
-    )
+    if run_id is not None:
+        run = wandb.init(
+            project="DQNsb3 - MDP - ObservableDeformedGridworld",
+            config=config,
+            sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+            monitor_gym=True,  # auto-upload the videos of agents playing the game
+            save_code=True,  # optional
+            run_id=run_id
+            resume="must"
+        )
+    else:
+        run = wandb.init(
+            project="DQNsb3 - MDP - ObservableDeformedGridworld",
+            config=config,
+            sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+            monitor_gym=True,  # auto-upload the videos of agents playing the game
+            save_code=True,  # optional
+        )
 
 
     # Save a checkpoint every 10000 steps
     checkpoint_callback = CheckpointCallback(
                             save_freq=250000,
-                            save_path=f"DQN_continous_{run.id}",
+                            save_path=f"agents/pretrained/MDP/DQN_continous_{run.id}",
                             name_prefix="rl_model",
                             save_replay_buffer=False,
                             save_vecnormalize=True,
@@ -86,11 +121,16 @@ def train_dqn(args):
 
 
     net_arch=[128, 128, 128]
-    model = DQN("MultiInputPolicy",env,batch_size=batch_size,gamma=gamma, 
+    if args.run_id is not None:
+        last_checkpoint = find_last_checkpoint(f"DQN_continous_{args.run_id}")
+        model = DQN.load(last_checkpoint,env=env,)
+    else:
+        model = DQN("MultiInputPolicy",env,batch_size=batch_size,gamma=gamma, 
                 target_update_interval=target_update, policy_kwargs=dict(net_arch=net_arch), verbose=1,
                 tensorboard_log=f"runs/{run.id}", device="cpu", learning_rate=lr,
                 train_freq=(10,"step"), gradient_steps=1)
-    model.learn(total_timesteps,progress_bar=True, callback=callbacks, log_interval=55)
+    
+    model.learn(total_timesteps,progress_bar=True, callback=callbacks, log_interval=55,reset_num_timesteps=False)
     model.save(f"agents/pretrained/MDP/DQN_continous_{run.id}")
     env.close()
     run.finish()
