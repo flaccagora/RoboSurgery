30acce0389ef18070c52f8430c025caf8ed18add
flaccagora <matteo.nunziante@gmail.com>
2024-12-20 20:07:15 +0100

    new repo structure

--- src/jobs/leonardo/MDP/PPO_MDP.sh
+++ src/jobs/leonardo/MDP/PPO_MDP.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH -A ICT24_DSSC_CPU
 #SBATCH -p dcgp_usr_prod
-#SBATCH --time 01:00:00     # format: HH:MM:SS
+#SBATCH --time 02:00:00     # format: HH:MM:SS
 #SBATCH -N 1                # 1 node
 #SBATCH --ntasks-per-node=4 # 4 tasks out of 112
-#SBATCH --mem=64000          # memory per node out of 494000MB (481GB)
-#SBATCH --job-name=PPO
-#SBATCH --output=PPO.out 
+#SBATCH --mem=128000          # memory per node out of 494000MB (481GB)
+#SBATCH --job-name=PPO_MDP
+#SBATCH --output=PPO_MDP.out 
+
+date
 
 cd ~/RoboSurgery
 source .rob/bin/activate
@@ -16,6 +18,6 @@ cd ./src
 export PYTHONPATH=$(pwd)
 export WANDB_MODE=offline
 
-python3 train/MDP/PPO.py
+python3 train/MDP/PPO_newenv.py --total_timesteps=20000000 --run_id="enh53x0u"
 
 date
